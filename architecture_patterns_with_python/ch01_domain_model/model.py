d56f1d5c8ef22f9fe8546aee43dfc5b6fc9ea301
Che-Min Lin <crosserclaws@gmail.com>
2023-11-18 22:15:09 +0800

    [feat] Batch can deallocate

--- architecture_patterns_with_python/ch01_domain_model/model.py
+++ architecture_patterns_with_python/ch01_domain_model/model.py
@@ -14,16 +14,29 @@ class Batch:
     def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]) -> None:
         self.reference = ref
         self.sku = sku
-        self.available_quantity = qty
         self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations: set[OrderLine] = set()
 
 
     def allocate(self, line: OrderLine):
-        if self.sku != line.sku:
-            raise ValueError(f'Bad argument: sku should be {self.sku}, but get {line.sku}')
-        if self.available_quantity < line.qty:
-            raise ValueError(f'Bad arguemnt: qty should >= {self.available_quantity}, but get {line.qty}')
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+
+    def deallocate(self,line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
 
 
     def can_allocate(self, line: OrderLine) -> bool:
