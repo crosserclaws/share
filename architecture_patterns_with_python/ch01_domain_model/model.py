65d527911960526c2edb23742b4407174d77e556
Che-Min Lin <crosserclaws@gmail.com>
2023-11-22 23:06:48 +0800

    [clean] Format

--- architecture_patterns_with_python/ch01_domain_model/model.py
+++ architecture_patterns_with_python/ch01_domain_model/model.py
@@ -5,9 +5,9 @@ from typing import Optional
 
 @dataclass(frozen=True)
 class OrderLine:
-        orderid: str
-        sku: str
-        qty: int
+    orderid: str
+    sku: str
+    qty: int
 
 
 class Batch:
@@ -18,26 +18,21 @@ class Batch:
         self._purchased_quantity = qty
         self._allocations: set[OrderLine] = set()
 
-
     def allocate(self, line: OrderLine):
         if self.can_allocate(line):
             self._allocations.add(line)
 
-
-    def deallocate(self,line: OrderLine):
+    def deallocate(self, line: OrderLine):
         if line in self._allocations:
             self._allocations.remove(line)
-
 
     @property
     def allocated_quantity(self) -> int:
         return sum(line.qty for line in self._allocations)
 
-
     @property
     def available_quantity(self) -> int:
         return self._purchased_quantity - self.allocated_quantity
-
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
