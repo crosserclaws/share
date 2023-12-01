fafde2f3846ddb9399b65809ffa535d39c356a98
Che-Min Lin <crosserclaws@gmail.com>
2023-12-01 22:02:36 +0800

    [ch04] Init

---
+++ architecture_patterns_with_python/ch04_first_use_case/model.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional
+
+
+def allocate(line: OrderLine, batches: list[Batch]) -> Batch:
+    try:
+        batch = next(b for b in sorted(batches) if b.can_allocate(line))
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f'Out of stock for sku {line.sku}')
+
+
+class OutOfStock(Exception):
+    pass
+
+
+@dataclass(unsafe_hash=True)
+class OrderLine:
+    orderid: str
+    sku: str
+    qty: int
+
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]) -> None:
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations: set[OrderLine] = set()
+
+    def __eq__(self, other) -> bool:
+        if type(self) is not type(other):
+            return NotImplemented
+        return self.reference == self.reference
+
+    def __gt__(self, other) -> bool:
+        if type(self) is not type(other):
+            return NotImplemented
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+    def __hash__(self) -> int:
+        return hash(self.reference)
+
+    def __repr__(self) -> str:
+        return f'<Batch {self.reference}>'
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
