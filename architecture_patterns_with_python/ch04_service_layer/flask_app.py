2711e0173cc0e24e4e8748f1d68ed43b162de7fc
Che-Min Lin <crosserclaws@gmail.com>
2023-12-07 23:31:58 +0800

    [ch04] Service layer

--- architecture_patterns_with_python/ch04_service_layer/flask_app.py
+++ architecture_patterns_with_python/ch04_service_layer/flask_app.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import sessionmaker
 import config
 import model
 import orm
+import services
 import repository
 
 orm.start_mappers()
@@ -16,21 +17,14 @@ app = Flask(__name__)
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
     session = get_session()
-    batches = repository.SqlAlchemyRepository(session).list()
+    repo = repository.SqlAlchemyRepository(session)
     line = model.OrderLine(
         request.json["orderid"], request.json["sku"], request.json["qty"]
     )
 
-    if not is_valid_sku(line.sku, batches):
-        return {"message": f"Invalid sku {line.sku}"}, 400
-
     try:
-        batchref = model.allocate(line, batches)
-    except model.OutOfStock as e:
+        batchref = services.allocate(line, repo, session)
+    except (model.OutOfStock, services.InvalidSku) as e:
         return {"message": str(e)}, 400
 
-    session.commit()
     return {"batchref": batchref}, 201
-
-def is_valid_sku(sku, batches):
-    return sku in {b.sku for b in batches}
