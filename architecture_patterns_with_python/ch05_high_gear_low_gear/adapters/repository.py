4e7e778302c03e14f2028b95a55afd595829468c
Che-Min Lin <crosserclaws@gmail.com>
2023-12-08 17:42:32 +0800

    [ch05] Init

---
+++ architecture_patterns_with_python/ch05_high_gear_low_gear/adapters/repository.py
@@ -0,0 +1,33 @@
+import abc
+from sqlalchemy.orm import Session
+
+from domain import model
+
+
+class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, batch: model.Batch):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference: str) -> model.Batch:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def list(self) -> list[model.Batch]:
+        raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session: Session) -> None:
+        super().__init__()
+        self.session = session
+
+    def add(self, batch: model.Batch):
+        self.session.add(batch)
+
+    def get(self, reference: str) -> model.Batch:
+        return self.session.query(model.Batch).filter_by(reference=reference).one()
+
+    def list(self) -> list[model.Batch]:
+        return self.session.query(model.Batch).all()
---
+++ architecture_patterns_with_python/ch05_high_gear_low_gear/service_layer/services.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+
+from adapters.repository import AbstractRepository
+from domain import model
+
+
+class InvalidSku(Exception):
+    pass
+
+
+def is_valid_sku(sku, batches):
+    return sku in {b.sku for b in batches}
+
+
+def allocate(line: model.OrderLine, repo: AbstractRepository, session: Session) -> str:
+    batches = repo.list()
+    if not is_valid_sku(line.sku, batches):
+        raise InvalidSku(f"Invalid sku {line.sku}")
+    batchref = model.allocate(line, batches)
+    session.commit()
+    return batchref
