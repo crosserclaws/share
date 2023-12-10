ee93e34648e5c8fd55b7ac5b2468241cacd02f70
Che-Min Lin <crosserclaws@gmail.com>
2023-12-10 16:54:51 +0800

    [ch05][refactor] Application service test
    Move all tests dependencies on the domain into one place

--- architecture_patterns_with_python/ch05_high_gear_low_gear/tests/unit/test_services.py
+++ architecture_patterns_with_python/ch05_high_gear_low_gear/tests/unit/test_services.py
@@ -1,4 +1,5 @@
-from typing import Iterable
+from datetime import date
+from typing import Iterable, Optional
 
 import pytest
 
@@ -20,18 +21,19 @@ class FakeRepository(repository.AbstractRepository):
     def list(self):
         return list(self._batches)
 
+    @staticmethod
+    def for_batch(ref: str, sku: str, qty: int, eta: Optional[date]=None):
+        return FakeRepository([model.Batch(ref, sku, qty, eta)])
+
 
 def test_returns_allocation():
-    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
-    repo = FakeRepository([batch])
-
+    repo = FakeRepository.for_batch("b1", "COMPLICATED-LAMP", 100, eta=None)
     result = services.allocate("o1", "COMPLICATED-LAMP", 10, repo, FakeSession())
     assert result == "b1"
 
 
 def test_error_for_invalid_sku():
-    batch = model.Batch("b1", "AREALSKU", 100, eta=None)
-    repo = FakeRepository([batch])
+    repo = FakeRepository.for_batch("b1", "AREALSKU", 100, eta=None)
 
     with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
         services.allocate("o1", "NONEXISTENTSKU", 10, repo, FakeSession())
@@ -45,8 +47,7 @@ class FakeSession:
 
 
 def test_commits():
-    batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
-    repo = FakeRepository([batch])
+    repo = FakeRepository.for_batch("b1", "OMINOUS-MIRROR", 100, eta=None)
     session = FakeSession()
 
     services.allocate("o1", "OMINOUS-MIRROR", 10, repo, session)
