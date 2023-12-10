3c6dd19bd2e58783de4f4512ce05b3ae0666afff
Che-Min Lin <crosserclaws@gmail.com>
2023-12-10 16:44:19 +0800

    [ch05][refactor] Application service allocate
    Application service API should not depend on domain model

--- architecture_patterns_with_python/ch05_high_gear_low_gear/service_layer/services.py
+++ architecture_patterns_with_python/ch05_high_gear_low_gear/service_layer/services.py
@@ -12,7 +12,8 @@ def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-def allocate(line: model.OrderLine, repo: AbstractRepository, session: Session) -> str:
+def allocate(orderid: str, sku: str, qty: int, repo: AbstractRepository, session: Session) -> str:
+    line = model.OrderLine(orderid, sku, qty)
     batches = repo.list()
     if not is_valid_sku(line.sku, batches):
         raise InvalidSku(f"Invalid sku {line.sku}")
--- architecture_patterns_with_python/ch05_high_gear_low_gear/tests/unit/test_services.py
+++ architecture_patterns_with_python/ch05_high_gear_low_gear/tests/unit/test_services.py
@@ -22,21 +22,19 @@ class FakeRepository(repository.AbstractRepository):
 
 
 def test_returns_allocation():
-    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
     batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
     repo = FakeRepository([batch])
 
-    result = services.allocate(line, repo, FakeSession())
+    result = services.allocate("o1", "COMPLICATED-LAMP", 10, repo, FakeSession())
     assert result == "b1"
 
 
 def test_error_for_invalid_sku():
-    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
     batch = model.Batch("b1", "AREALSKU", 100, eta=None)
     repo = FakeRepository([batch])
 
     with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
-        services.allocate(line, repo, FakeSession())
+        services.allocate("o1", "NONEXISTENTSKU", 10, repo, FakeSession())
 
 
 class FakeSession:
@@ -47,10 +45,9 @@ class FakeSession:
 
 
 def test_commits():
-    line = model.OrderLine("o1", "OMINOUS-MIRROR", 10)
     batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
     repo = FakeRepository([batch])
     session = FakeSession()
 
-    services.allocate(line, repo, session)
+    services.allocate("o1", "OMINOUS-MIRROR", 10, repo, session)
     assert session.committed is True
